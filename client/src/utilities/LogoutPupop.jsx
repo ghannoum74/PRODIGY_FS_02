6980b8fcaccb66623347265f7a5067857548008b
ghannoum74 <abdelrahmangh74@gmail.com>
2024-09-26 02:29:30 +0300

    create the logout pupop with the functionalities for the button

---
+++ client/src/utilities/LogoutPupop.jsx
@@ -0,0 +1,45 @@
+import { AnimatePresence, motion } from "framer-motion";
+import PropTypes from "prop-types";
+import { useNavigate } from "react-router-dom";
+
+const LogoutPupop = ({ setLogoutPupop }) => {
+  const navigate = useNavigate();
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    localStorage.removeItem("id");
+    localStorage.removeItem("user-authenticate");
+    navigate("/");
+  };
+  return (
+    <div className="blured-backg">
+      <AnimatePresence>
+        <motion.div
+          initial={{ scale: 0.5, opacity: 0 }}
+          animate={{ scale: 1, opacity: 1 }}
+          exit={{ scale: 0, opacity: 0 }}
+          transition={{ duration: 0.2 }}
+          className="container-authPage"
+        >
+          <h5>Are you sure do you want leave use?</h5>
+          <div className="btn-logout-pupop">
+            <button className="switch-bttn yes" onClick={handleLogout}>
+              Yes
+            </button>
+            <button
+              className="switch-bttn no"
+              onClick={() => setLogoutPupop(false)}
+            >
+              No
+            </button>
+          </div>
+        </motion.div>
+      </AnimatePresence>
+    </div>
+  );
+};
+
+LogoutPupop.propTypes = {
+  setLogoutPupop: PropTypes.func,
+};
+
+export default LogoutPupop;
