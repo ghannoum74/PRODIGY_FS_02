e5ed98d3c885abb576f9081bb3e8e79b0299bdfa
ghannoum74 <abdelrahmangh74@gmail.com>
2024-09-26 02:15:06 +0300

    create api delete task as admin

---
+++ server/controlers/administrator/tasks/deleteAdminTask.js
@@ -0,0 +1,25 @@
+const Tasks = require("../../../models/tasks");
+
+const deleteAdminTask = async (req, res) => {
+  try {
+    const taskId = req.params.taskId;
+    // Ensure the task belongs to the authenticated user
+    const task = await Tasks.findOneAndDelete({
+      _id: taskId,
+    });
+
+    if (!task) {
+      return res
+        .status(404)
+        .json({ message: "Task not found or not authorized" });
+    }
+
+    res.status(200).json({ message: "Task deleted successfully" });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: "An error occurred while deleting the task" });
+  }
+};
+
+module.exports = { deleteAdminTask };
