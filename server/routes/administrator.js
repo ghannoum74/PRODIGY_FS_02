5b1ce655087b6bdfebf93387c306c893cbed84c1
ghannoum74 <abdelrahmangh74@gmail.com>
2024-09-26 02:22:12 +0300

    create admin route

--- server/routes/administrator.js
+++ server/routes/administrator.js
@@ -1,23 +1,73 @@
 const express = require("express");
 const { isUserAuthenticated } = require("../middlewears/isUserAuthenticated");
 const { isAdmin } = require("../middlewears/isAdmin");
-const { getAllTasks } = require("../controlers/administrator/getAllTasks");
+
+const { loginAdmin } = require("../controlers/administrator/login");
+
 const {
   deleteAdminTask,
-} = require("../controlers/administrator/deleteAdminTask");
+} = require("../controlers/administrator/tasks/deleteAdminTask");
 const {
   updateAdminTask,
-} = require("../controlers/administrator/updateAdminTask");
+} = require("../controlers/administrator/tasks/updateAdminTask");
+const { getAllData } = require("../controlers/administrator/getAllData");
+const {
+  deleteAdminUser,
+} = require("../controlers/administrator/user/deleteAdminUser");
+const {
+  updateAdminUser,
+} = require("../controlers/administrator/user/updateAdminUser");
+const {
+  createAdminUser,
+} = require("../controlers/administrator/user/createAdminUser");
+const {
+  createAdminTask,
+} = require("../controlers/administrator/tasks/createAdminTask");
+
 const router = express.Router();
 
-router.get("/get-all", isUserAuthenticated, isAdmin, getAllTasks);
+router.post("/login", loginAdmin);
+router.get("/get-all", isUserAuthenticated, isAdmin, getAllData);
+
+router.post(
+  "/tasks/create-task",
+  isUserAuthenticated,
+  isAdmin,
+  createAdminTask
+);
 router.delete(
-  "/delete-task/:taskId",
+  "/tasks/delete-task/:taskId",
   isUserAuthenticated,
   isAdmin,
   deleteAdminTask
 );
 
-router.patch("/update-task/:id", isUserAuthenticated, isAdmin, updateAdminTask);
+router.patch(
+  "/tasks/update-task/:id",
+  isUserAuthenticated,
+  isAdmin,
+  updateAdminTask
+);
+
+router.post(
+  "/users/create-user",
+  isUserAuthenticated,
+  isAdmin,
+  createAdminUser
+);
+
+router.patch(
+  "/users/update-user/:userId",
+  isUserAuthenticated,
+  isAdmin,
+  updateAdminUser
+);
+
+router.delete(
+  "/users/delete-user/:userId",
+  isUserAuthenticated,
+  isAdmin,
+  deleteAdminUser
+);
 
 module.exports = router;
