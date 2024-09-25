366333d1a93f6b718daec4f8def3e26037d90786
ghannoum74 <abdelrahmangh74@gmail.com>
2024-09-26 02:28:04 +0300

    create the data for the inputs authentication

---
+++ client/src/utilities/inputsData.js
@@ -0,0 +1,65 @@
+export const inputsData = [
+  {
+    id: 1,
+    name: "firstName",
+    type: "text",
+    className: "input",
+    messageerror:
+      "Sorry, only letters (a-z), numbers (0-9), and space are allowed.",
+    match: /^[a-zA-Z][a-zA-Z0-9 ]{3,}$/,
+    label: "First Name",
+    placeholder: "",
+  },
+  {
+    id: 2,
+    name: "lastName",
+    type: "text",
+    className: "input",
+    messageerror:
+      "Sorry, only letters (a-z), numbers (0-9), and periods (.) are allowed.",
+    match: /^[a-zA-Z][a-zA-Z0-9 ]{3,}$/,
+
+    label: "Last Name",
+    placeholder: "",
+  },
+  {
+    id: 3,
+    name: "birthday",
+    type: "date",
+    className: "input",
+    label: "Birthday",
+    messageerror: " Sorry, Birthday field is required",
+    placeholder: "",
+  },
+  {
+    id: 4,
+    name: "email",
+    type: "email",
+    className: "input",
+    messageerror: " Email should match format:Example123@gmail.com",
+    match: /^[a-zA-Z][a-zA-Z0-9]{2,}@(gmail|hotmail).com$/,
+    label: "Email",
+    placeholder: "",
+  },
+  {
+    id: 5,
+    name: "password",
+    type: "password",
+    className: "input",
+    messageerror:
+      " Password should contain letter(A-z),number(0-9),charactere(!#)",
+    match: /(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/,
+    label: " Password",
+    placeholder: "",
+  },
+  {
+    id: 6,
+    name: "confirmPassword",
+    type: "password",
+    className: "input",
+    messageerror: " confirm Password should match the password",
+    match: "(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$",
+    label: "confirm Password",
+    placeholder: "",
+  },
+];
