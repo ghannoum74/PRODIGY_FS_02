79a072b0ca40979e31acb07f2bad2f2be1cffafd
ghannoum74 <abdelrahmangh74@gmail.com>
2024-09-26 02:21:30 +0300

    create authorization middleWeare isAdmin

--- server/middlewears/isAdmin.js
+++ server/middlewears/isAdmin.js
@@ -1,8 +1,8 @@
 const isAdmin = (req, res, next) => {
-  if (!req.user || !req.user.isAdmin) {
+  if (!req.user || !req.user.Admin) {
     return res
       .status(403)
-      .json({ message: "Forbidden: You do not have access" });
+      .json({ message: "Forbidden: You do not have access!" });
   }
   next();
 };
