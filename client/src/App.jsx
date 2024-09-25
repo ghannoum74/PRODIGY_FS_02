4a6bc2ec1324cf25a873fb3d36983391f80bd831
ghannoum74 <abdelrahmangh74@gmail.com>
2024-09-26 02:26:41 +0300

    modify the app component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,20 @@
 import "./CSS/App.css";
 import { Route, BrowserRouter, Routes } from "react-router-dom";
 import WelcomePage from "./pages/WelcomePage";
+import MainContainer from "./components/mainContainer";
+import HomePage from "./pages/HomePage";
+import AdminPage from "./pages/AdminPage";
+
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<WelcomePage />} />
-      </Routes>
+      <MainContainer>
+        <Routes>
+          <Route path="/" element={<WelcomePage />} />
+          <Route path="/homePage" element={<HomePage />} />
+          <Route path="/admin" element={<AdminPage />} />
+        </Routes>
+      </MainContainer>
     </BrowserRouter>
   );
 }
